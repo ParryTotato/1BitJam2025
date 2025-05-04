9d93ecebb9c0a43cb10ecf0cfe64dfc0c5e12675
Prateek Bansal <bansalparry@gmail.com>
2025-05-04 01:22:25 -0400

    resized images down from 64x64 to 16x16 and redid collision and movement to align to grid

--- Scenes/Box/box.gd
+++ Scenes/Box/box.gd
@@ -1,17 +1,28 @@
 extends CharacterBody2D
 
 signal push_completed
+
+var grid_size = 16
 var being_pushed = false
 
-func push(direction: Vector2, pusher: Node):
+func push(direction: Vector2, pusher: Node) -> bool:
 	if being_pushed:
 		return false
 	being_pushed = true
-	var tween = create_tween()
-	tween.tween_property(self, "position", position + direction * 64, 0.5)
-	tween.tween_property(self, "scale", Vector2.ONE, 0.1).from(Vector2(1.1, 0.9))
-	tween.finished.connect(_on_push_complete, CONNECT_ONE_SHOT)
 	
+	var target_pos = position + (direction.normalized() * grid_size)
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(position, target_pos, collision_mask, [self, pusher])
+	var result = space_state.intersect_ray(query)
+	
+	if result:
+		being_pushed = false
+		return false
+	
+	# Move the box
+	var tween = create_tween()
+	tween.tween_property(self, "position", target_pos, 0.2)
+	tween.finished.connect(_on_push_complete)
 	return true
 
 func _on_push_complete():
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,47 +1,75 @@
 extends CharacterBody2D
 
-@export var speed = 150
-var last_direction = ""
+@export var move_speed:= 0.2
+
+#Flags
+var grid_size := 16
+var is_moving = false
 var is_pushing = false
+var last_direction = "idle"
+var target_position : Vector2
 
 # Variables that can be increased via upgrades
 var push_force = 64
 
-func _physics_process(delta):
-	if is_pushing:
+func _ready():
+	target_position = position
+
+func _physics_process(_delta):
+	if is_moving or is_pushing:
 		return
 		
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_dir.normalized() * speed
-	
-	movement_handling(input_dir)
-	var collision = move_and_collide(velocity * delta)
-	
-	if collision:
-		var node = collision.get_collider()
-		if node.has_method("push"):
-			var push_direction = velocity.normalized()
-			if node.push(push_direction * push_force * delta, self):
+	var input_dir := Vector2.ZERO
+	if Input.is_action_just_pressed("ui_left"):
+		input_dir.x = -1
+	elif Input.is_action_just_pressed("ui_right"):
+		input_dir.x = 1
+	elif Input.is_action_just_pressed("ui_up"):
+		input_dir.y = -1
+	elif Input.is_action_just_pressed("ui_down"):
+		input_dir.y = 1
+		
+	if input_dir != Vector2.ZERO:
+		var new_target = target_position + (input_dir * grid_size)
+		var space_state = get_world_2d().direct_space_state
+		var query = PhysicsRayQueryParameters2D.create(position, new_target, collision_mask, [self])
+		var result = space_state.intersect_ray(query)
+		
+		# No collision, move normally
+		if not result:
+			target_position = new_target
+			is_moving = true
+			var tween = create_tween()
+			tween.tween_property(self, "position", target_position, move_speed)
+			tween.finished.connect(_on_move_complete)
+			_play_move_animation(input_dir)
+		# Collision, push time
+		elif result.collider.has_method("push"):
+			var push_direction = input_dir
+			if result.collider.push(push_direction * push_force, self):
 				is_pushing = true
-				node.push_completed.connect(reset_push, CONNECT_ONE_SHOT)
-
-func movement_handling(input_dir: Vector2):
-	if input_dir.x > 0 and last_direction != "right":
-		$AnimatedSprite2D.play("right")
-		last_direction = "right"
-	elif input_dir.x < 0 and last_direction != "left":
-		$AnimatedSprite2D.play("left")
-		last_direction = "left"
-	elif input_dir.y < 0 and last_direction != "up":
-		$AnimatedSprite2D.play("up")
-		last_direction = "up"
-	elif input_dir.y > 0 and last_direction != "down":
-		$AnimatedSprite2D.play("down")
-		last_direction = "down"
-	elif input_dir == Vector2.ZERO and last_direction != "idle":
+				result.collider.push_completed.connect(_on_push_complete, CONNECT_ONE_SHOT)
+	elif last_direction != "idle":
 		$AnimatedSprite2D.stop()
 		$AnimatedSprite2D.frame = 0
 		last_direction = "idle"
 
-func reset_push():
+func _play_move_animation(input_dir: Vector2):
+	if input_dir.x > 0 and last_direction != "right": 
+		$AnimatedSprite2D.play("right")
+		last_direction = "right"
+	elif input_dir.x < 0 and last_direction != "left": 
+		$AnimatedSprite2D.play("left")
+		last_direction = "left"
+	elif input_dir.y < 0 and last_direction != "up": 
+		$AnimatedSprite2D.play("up")
+		last_direction = "up"
+	elif input_dir.y > 0 and last_direction != "down": 
+		$AnimatedSprite2D.play("down")
+		last_direction = "down"
+
+func _on_move_complete():
+	is_moving = false
+
+func _on_push_complete():
 	is_pushing = false

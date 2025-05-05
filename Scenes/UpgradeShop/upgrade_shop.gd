7dbe8eb81a66d7e8b4108d8031b7a4305f2b413d
Born2Crit <Born2Crit@gmail.com>
2025-05-05 03:49:46 -0500

    refactor: replacing global game with signal bus

--- Scenes/UpgradeShop/upgrade_shop.gd
+++ Scenes/UpgradeShop/upgrade_shop.gd
@@ -1,21 +1,72 @@
 class_name UpgradeShop
 extends Control
 
+var coin_count: int = 50
+var coin_multiplier: int = 0
 
-func _process(delta: float) -> void:
-	$CoinCount.text = "Coin Count: " + str(GlobalGame.coins)
-	$PlayerStats/Timer.text = "Timer Duration: " + str(GlobalGame.upgradeTree["timer_duration"])
-	$PlayerStats/CoinMulti.text = "Coin Multi: " + str(GlobalGame.upgradeTree["coin_multi"])
+var upgrades: Dictionary = {
+	"timer_duration": {"tier": 0, "cap": 10},
+	"coin_multiplier": {"tier": 0, "cap": 10},
+	"kick": {"tier": 0, "cap": 1},
+	"magnet": {"tier": 0, "cap": 1},
+	"translocator": {"tier": 0, "cap": 1}
+}
+
+@onready var coin_count_label: Label = $MarginContainer/CoinCount
 
 
-func _on_timer_upgrade_pressed() -> void:
-	if validate_coin_count():
-		GlobalGame.coins -= 1
-		GlobalGame.upgradeTree["timer_duration"] += 2
+func _ready() -> void:
+	Messenger.connect("coin_added", _on_coin_added)
 
 
-func validate_coin_count() -> bool:
-	if GlobalGame.coins > 0:
-		return true
-	 
-	return false
+func _on_timer_duration_pressed() -> void:
+	if coin_count >= 2 and upgrades["timer_duration"]["tier"] <= upgrades["timer_duration"]["cap"]:
+		spend_coins(2)
+		upgrades["timer_duration"]["tier"] += 1
+		Messenger.timer_duration_upgraded.emit()
+
+
+func _on_coin_multiplier_pressed() -> void:
+	if coin_count >= 2:
+		spend_coins(2)
+		coin_multiplier += 2
+
+
+func _on_kick_pressed() -> void:
+	if coin_count >= 4:
+		spend_coins(4)
+		Messenger.kick_upgraded.emit()
+
+
+func _on_magnet_pressed() -> void:
+	if coin_count >= 4:
+		spend_coins(4)
+		Messenger.magnet_upgraded.emit()
+
+
+func _on_translocator_pressed() -> void:
+	if coin_count >= 4:
+		spend_coins(4)
+		Messenger.translocator_upgraded.emit()
+
+
+func _on_refund_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_coin_added() -> void:
+	if coin_multiplier:
+		coin_count += 1 * coin_multiplier
+	else:
+		coin_count += 1
+		
+	coin_count_label.text = str(coin_count)
+
+
+func spend_coins(amount: int) -> void:
+	coin_count -= amount
+	coin_count_label.text = str(coin_count)
---
+++ messenger.gd
@@ -0,0 +1,10 @@
+extends Node
+
+# Coin
+signal coin_added
+
+# UpgradeShop
+signal timer_duration_upgraded
+signal kick_upgraded
+signal magnet_upgraded
+signal translocator_upgraded
